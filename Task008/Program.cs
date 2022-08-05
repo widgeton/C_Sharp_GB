efc5eaa843304baf59c30aee18f16a6a4b4dbd8e
Anton Vashenko <vashchenko-96@mail.ru>
2022-08-05 13:37:39 +0300

    Add Task 6 even numbers in range from 1 to N

---
+++ Task008/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+try
+{
+    Console.Write("Введите натуральное число: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if (num <= 0) throw new Exception();
+
+    if (num == 1) Console.WriteLine("Четные числа отсутствуют.");
+    else
+    {
+        Console.Write($"Четные числа от 1 до {num}: ");
+
+        for (int i = 2; i <= num; i += 2) Console.Write($"{i} ");
+
+        Console.WriteLine();
+    }
+}
+
+catch
+{
+    Console.WriteLine("Вы ввели некорректное значение.");
+}
