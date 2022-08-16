2dc2eb57662bee666ab8a2b3a815cc9d73021c62
Anton Vashenko <vashchenko-96@mail.ru>
2022-08-16 15:50:02 +0300

    Add Task 27 Sum of figures in number

---
+++ Task427/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumFigures(int num)
+{
+    int sum = default;
+    
+    if(num < 0) num *= -1;    
+    while(num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+Console.Write("Введите целое число: ");
+int n = int.Parse(Console.ReadLine());
+
+int result = SumFigures(n);
+Console.WriteLine($"Сумма цифр в числе {n} равно {result}");
