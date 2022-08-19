e95072a5b0f93c304603cfbf896a3a30139220a8
Anton Vashenko <vashchenko-96@mail.ru>
2022-08-19 15:23:40 +0300

    Task 36 Sum elements of array with odd index

---
+++ Task536/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36
+// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int SumElementsWithOddIndex (int[] array)
+{
+    int sum = default;
+
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+
+    return sum;
+}
+
+int[] arr = CreateArray(4, -10, 100);
+PrintArray(arr);
+int result = SumElementsWithOddIndex(arr);
+Console.WriteLine($"Сумма элементов с нечетными индексами равна {result}");
