c28255d436a82eb3eadca544f25591ed442579bc
Anton Vashenko <vashchenko-96@mail.ru>
2022-08-23 15:38:06 +0300

    Task 41 and Task 43

---
+++ Task641/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41:
+// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[] StringArrayToInt(string[] array)
+{
+    int[] arr = new int[array.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = int.Parse(array[i]);
+    }
+    return arr;
+}
+
+int PositiveNumbersInArray(int[] array)
+{
+    int count = 0;
+    foreach (var i in array)
+    {
+        if(i > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите ряд целых чисел через пробел: ");
+string[] numbers = Console.ReadLine().Split();
+int[] nums = StringArrayToInt(numbers);
+
+Console.WriteLine($"Пользователь ввел {PositiveNumbersInArray(nums)} чисел больше нуля");
---
+++ Task643/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 43:
+// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// k1 * x + b1 = k2 * x + b2
+// k1 * x - k2 * x = b2 - b1
+// x(k1 - k2) = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+
+Console.Write("Введите значение b для первой прямой: ");
+int b1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение k для первой прямой: ");
+int k1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение b для второй прямой: ");
+int b2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение k для второй прямой: ");
+int k2 = int.Parse(Console.ReadLine());
+
+double[] FindCrossingPoint(int a1, int c1, int a2, int c2)
+{
+    double[] xy = new double[2];
+
+    xy[0] = (double)(a2 - a1) / (c1 - c2);
+    xy[1] = c1 * xy[0] + a1;
+
+    return xy;
+}
+
+double[] result = FindCrossingPoint(b1, k1, b2, k2);
+Console.WriteLine($"({result[0]}; {result[1]})");
