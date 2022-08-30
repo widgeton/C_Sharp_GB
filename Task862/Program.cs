429b2c1e3ec0291647dfbe61ae6535ac059b9d3a
Anton Vashenko <vashchenko-96@mail.ru>
2022-08-30 17:12:01 +0300

    Add Task 862 Snake filling matrix

---
+++ Task862/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] CreateSnakeFillingMatrix(int row, int col)
+{
+    int[,] matrix = new int[row, col];
+    int num = 1;
+
+    for (int forI = 0, forJ = 0; num <= row * col; forI++, forJ++)
+    {
+        int i = forI;
+        int j = forJ;
+        for (; j < col - 1 - forJ && num <= row * col; j++, num++)
+            matrix[i, j] = num;
+
+        for (; i < row - 1 - forI && num <= row * col; i++, num++)
+            matrix[i, j] = num;
+
+        for (; j > forJ && num <= row * col; j--, num++)
+            matrix[i, j] = num;
+
+        for (; i > forI && num <= row * col; i--, num++)
+            matrix[i, j] = num;
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("[");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 3}, ");
+      else Console.Write($"{matrix[i, j], 3}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+  }
+}
+
+int[,] mtrx = CreateSnakeFillingMatrix(10, 5);
+PrintMatrix(mtrx);
