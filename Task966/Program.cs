0237dfc7c5cc5ac56d8ff6484284e2e7ce62e410
Anton Vashenko <vashchenko-96@mail.ru>
2022-09-04 09:52:26 +0300

    Add Task 966 Recursion output sum of numbers in range

---
+++ Task966/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66:
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumInRange(int a, int b)
+{
+    int sum = default;
+    if(a < b)
+    {
+        sum = a + SumInRange(a + 1, b);
+    }
+    if(a > b)
+    {
+        sum = a + SumInRange(a - 1, b);
+    }
+    if(a == b)
+    {
+        sum += a;
+    }
+    return sum;
+}
+
+System.Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine());
+
+System.Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"Сумма чисел от {n} до {m} = {SumInRange(n, m)}");
